fbcde98ba93953fd30e3841e8b65d801349804d9
Sean Cribbs <seancribbs@gmail.com>
2011-03-13 17:39:40 -0400

    Dirty merge of Graeme's fixes.

--- test/test_memoization.erl
+++ test/test_memoization.erl
@@ -3,31 +3,31 @@
 -include_lib("eunit/include/eunit.hrl").
 
 setup_memo_test() ->
-    neotoma_peg:setup_memo(),
+    peg_includes:setup_memo(),
     ?assertNot(undefined == ets:info(get(parse_memo_table))),
-    neotoma_peg:release_memo().
+    peg_includes:release_memo().
 
 release_memo_test() ->
-    neotoma_peg:setup_memo(),
-    neotoma_peg:release_memo(),
+    peg_includes:setup_memo(),
+    peg_includes:release_memo(),
     ?assertEqual(undefined, ets:info(get(parse_memo_table))).
 
 step_memo_test() ->
-    neotoma_peg:setup_memo(),
-    Result = neotoma_peg:p(<<"abcdefghi">>, {{line,1},{column,1}}, anything, neotoma_peg:p_anything()),
+    peg_includes:setup_memo(),
+    Result = peg_includes:p(<<"abcdefghi">>, {{line,1},{column,1}}, anything, peg_includes:p_anything()),
     ?assertEqual({<<"a">>, <<"bcdefghi">>, {{line,1},{column,2}}}, Result),
-    Result2 = neotoma_peg:p(<<"abcdefghi">>, {{line,1},{column,1}}, anything, fun(_) ->
+    Result2 = peg_includes:p(<<"abcdefghi">>, {{line,1},{column,1}}, anything, fun(_) ->
                                                                                   throw(bork) end),
     ?assertEqual(Result, Result2),
-    neotoma_peg:release_memo().
+    peg_includes:release_memo().
 
 concurrent_memo_test() ->
     Me = self(),
-    Him = spawn(fun() -> Me ! neotoma_peg:setup_memo(), receive _ -> ok after 10000 -> ok end end),
-    MyTid = neotoma_peg:setup_memo(),
+    Him = spawn(fun() -> Me ! peg_includes:setup_memo(), receive _ -> ok after 10000 -> ok end end),
+    MyTid = peg_includes:setup_memo(),
     receive
         Tid -> ?assertNot(Tid == MyTid),
                Him ! ok
     after 10000 -> ok
     end,
-    neotoma_peg:release_memo().
+    peg_includes:release_memo().
